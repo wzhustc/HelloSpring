2d93e097e53f7432090556385b809b2bb5c2f0ef
zhangwei <505659671@qq.com>
2016-10-12 16:23:50 +0800

    Spring tutorial 第一个例子HelloSpring

---
+++ src/com/springtutorials/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.springtutorials;
+
+public class HelloWorld {
+	private String message;
+
+	public void getMessage() {
+		System.out.println("your message :" + message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
